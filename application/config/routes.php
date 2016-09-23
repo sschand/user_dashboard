60746c413a5e7a4ffafbbffaba24f8ce4e6a48c8
Sharol Chand <sharolchand@gmail.com>
2016-09-22 20:24:07 -0700

    in order for codeigniter to successfully find controllers, lowercase controller name in routes!!

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,12 +1,12 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $route['default_controller'] = "main";
-$route['signin'] = "Main/signin";
-$route['register'] = "Main/register";
-$route['users/new'] = "Users/new_user";
-$route['users/edit'] = "Users/edit";
-$route['users/edit/(:any)'] = "Users/edit_user/$1";
-$route['users/remove/(:num)'] = "Users/remove/$1";
+$route['signin'] = "main/signin";
+$route['register'] = "main/register";
+$route['users/new'] = "users/new_user";
+$route['users/edit'] = "users/edit";
+$route['users/edit/(:any)'] = "users/edit_user/$1";
+$route['users/remove/(:num)'] = "users/remove/$1";
 $route['404_override'] = '';
 
 //end of routes.php
